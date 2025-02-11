c2161273e851c92efd92448115ce1ee177bd82be
DWH <2521248869@qq.com>
2025-02-11 17:07:58 +0800

    initial proj

---
+++ src/lang_items.rs
@@ -0,0 +1,16 @@
+//! The panic handler
+
+use crate::sbi::shutdown;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+/// panic handler
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+            println!("[kernel] Panicked at {}:{} {}", location.file(), location.line(), info.message());
+    } else {
+            println!("[kernel] Panicked: {}", info.message());
+    }
+    shutdown();
+}
+
